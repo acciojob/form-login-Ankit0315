3f48c01e0b2e0c351f0508fca429c1c173bf9be5
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-12 21:24:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+const form = document.getElementById('form1');
+	const name=form.elements['fname'].value;
+	const lastname=form.elements['lname'].value;
+	
+	alert(name + lastname);
+	
 }
